68f5d56908f7bc4c6dc9b7d8cd1a507bf09dfad3
Titiprihartati <titiprihartati@gmail.com>
2025-03-09 20:50:21 +0700

    dashboard1.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyOkD7xUyWfRD/KeBEFp62Zo",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
